11e13e3f193855879007ec05d0addcc16323adde
rishingx <rishing04@gmail.com>
2024-07-18 23:55:25 +0530

    Added solutions for A1 and A2

--- code/solns/S1.pl
+++ code/solns/S1.pl
@@ -1,5 +1,7 @@
 /* === Assignment 1: Implementation of Sets as Lists with no duplicates === */
 
+ :- discontiguous mem/2.
+
 /* === Memership Function: mem === */
 
 mem(X, [X|_])               :-      !.
@@ -44,7 +46,7 @@ powerI([X|R], P)            :-      powerI(R, P1), mapcons(X, P1, P2), append(P2
 
 /* === Intersection Function: interI === */
 
-interI(S1, [], [])          :-      !.
+interI(_, [], [])           :-      !.
 interI(S1, [X|R], [X|Z])    :-      interI(S1, R, Z), mem(X, S1).
 interI(S1, [X|R], Z)        :-      interI(S1, R, Z), notmem(X, S1).
 
@@ -53,3 +55,22 @@ interI(S1, [X|R], Z)        :-      interI(S1, R, Z), notmem(X, S1).
 diffI([], _, [])            :-      !.
 diffI([X|R], S2, [X|Z])     :-      diffI(R, S2, Z), interI([X|R], S2, L), notmem(X, L).
 diffI([X|R], S2, Z)         :-      diffI(R, S2, Z), interI([X|R], S2, L), mem(X, L).
+
+/* === Inverse Relation: inv === */
+
+mem( (Y,X), inv(R) )        :-      mem( (X,Y), R ).
+
+/* === Reflexive Closure: refclos === */
+
+mem( (X,X), refclos(_, S) )    :-      mem( X, S ).
+mem( (X,Y), refclos(R, _) )    :-      mem( (X,Y), R ).
+
+/* === Symmetric Closure: symclos === */
+
+mem( (X,Y), symclos(R, _) )    :-      mem( (X,Y), R).
+mem( (X,Y), symclos(R, _) )    :-      mem( (X,Y), inv(R) ).
+
+/* === Transitive Closure: transclos === */
+
+mem( (X,Y), transclos(R, _) )   :-      mem( (X,Y), R).
+mem( (X,Z), transclos(R, S) )   :-      mem( (X,Y), R), mem( (Y,Z), transclos(R, S) ).
